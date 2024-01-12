669a442034cc936d62b18d87a8896cba74e72b89
Bea Obiero <bea.obiero@gmail.comgit config --global user.email bea.obiero@gmail.comgit config --global user.email + + bea.obiero@gmail.com>
2024-01-12 13:37:08 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  function initializeTaskForm(){
+    const form = document.getElementById('taskForm');
+
+    function handleFormSubmission(event) {
+      event.preventDefault();
+
+
+      const taskInput = document.getElementById('taskInput');
+      const taskValue = taskInput.value;
+      const taskList = document.getElementById('taskList');
+      const newTaskItem = document.createElement('li');
+      newTaskItem.textContent = taskValue;
+      taskList.appendChild(newTaskItem);
+
+      //clear input field
+      taskInput.value = '';
+
+    }
+
+    form.addEventListener('submit', handleFormSubmission);
+
+  }
+
+  initializeTaskForm();
+  
 });
